e50dadda80471c5055b95c7da0dc08acf2d608e2
shashwat0802 <shashwatgupta4656@gmail.com>
2023-04-07 12:36:34 +0530

    change routing

--- src/pages/RoadNetwork.js
+++ src/pages/RoadNetwork.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 export default function RoadNetwork() {
   return (
-    <div className="container d-flex align-items-center flex-column" style={{height:'100%'}}>
+    <div>
+        <Sidebar/>
+        <div className="container d-flex align-items-center flex-column" style={{height:'100%'}}>
         <button type="button" className="btn btn-primary align-self-end my-4">New</button>
+    </div>
     </div>
   )
 }
